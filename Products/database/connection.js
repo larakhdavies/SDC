1fd1fbcf5bbeace70ff93ca364c8f02ffc7dba03
larakhdavies <lara.kh.davies@gmail.com>
2021-03-18 12:43:28 -1000

    add query to get product by id wiht features

--- Products/database/connection.js
+++ Products/database/connection.js
@@ -18,6 +18,30 @@ client.connect((err) => {
   }
 });
 
+const getProductsById = function (req, res) {
+  let id = Number(req.params.product_id)
+  let featuresQuery = `SELECT
+  features.feature,
+  features.valueAttr
+  FROM products_general
+  INNER JOIN features ON products_general.product_id=features.product_id
+  where (products_general.product_id = ${id});`
+  let productsQuery = `SELECT * FROM products_general WHERE product_id=${id}`
+  client.query(featuresQuery)
+  .then((data) => {
+    client.query(productsQuery)
+    .then((data2) => {
+      let featuresArr = data.rows;
+      data2.rows[0]['features'] = featuresArr;
+      res.send(data2.rows);
+    })
+  })
+  .catch((error) => {
+    res.send(error)
+  })
+}
+
 module.exports = {
   client,
+  getProductsById,
 }
--- Products/server/index.js
+++ Products/server/index.js
@@ -4,19 +4,19 @@ const app = express()
 const port = 3000
 
 app.get('/products', (req, res) => {
-  debugger;
   let page;
   let count;
   (req.query.page ? page = Number(req.query.page) : page = 1);
   (req.query.count ? count = Number(req.query.count) : count = 5);
   let startProductId = (page-1) * count;
   let sql = `SELECT * FROM products_general WHERE product_id>${startProductId} LIMIT ${count}`
-  debugger;
   connection.client.query(sql, (error, results) => {
-    debugger;
     res.send(results.rows)
   })
 })
+
+
+app.get('/products/:product_id', connection.getProductsById);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
